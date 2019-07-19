b06ee9df9338ea75027f55749f678e1e9c3f4d57
Marcelo <qw3xvf@gmail.com>
2019-07-19 16:05:11 +0900

    get the resource based on the user id

--- apps/base_api/lib/base_api/guardian.ex
+++ apps/base_api/lib/base_api/guardian.ex
@@ -1,6 +1,8 @@
 defmodule BaseApi.Guardian do
   use Guardian, otp_app: :base_api
 
+  alias Database.Accounts
+
   def subject_for_token(resource, _claims) do
     sub = to_string(resource.id)
     {:ok, sub}
@@ -11,7 +13,7 @@ defmodule BaseApi.Guardian do
 
   def resource_from_claims(claims) do
     id = claims["sub"]
-    resource = Accounts.get_user(%{:sub => id})
+    resource = Accounts.get_user!(id)
     {:ok,  resource}
   end
   def resource_from_claims(_claims) do
