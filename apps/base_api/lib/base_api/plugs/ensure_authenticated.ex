ee7adcbe7a8b27f6768d22fa50bc9ea573df6d0c
Marcelo <qw3xvf@gmail.com>
2019-07-19 16:05:35 +0900

    fix: specify the guardian module

--- apps/base_api/lib/base_api/plugs/ensure_authenticated.ex
+++ apps/base_api/lib/base_api/plugs/ensure_authenticated.ex
@@ -1,5 +1,5 @@
 defmodule BaseApi.Plugs.EnsureAuthenticatedPlug do
-  use Guardian.Plug.Pipeline, otp_app: :base_api
+  use Guardian.Plug.Pipeline, module: BaseApi.Guardian, otp_app: :base_api
 
   plug Guardian.Plug.VerifyHeader, realm: "Bearer"
   plug Guardian.Plug.EnsureAuthenticated
