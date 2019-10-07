e44f18f97c43f4e8d562d8e6f83377a699d0bc65
Megumiso <github@megumiso.com>
2019-10-07 12:17:54 +0900

    second commit

--- apps/database/priv/repo/migrations/20190910081850_create_user_servers.exs
+++ apps/database/priv/repo/migrations/20190910081850_create_user_servers.exs
@@ -2,6 +2,14 @@ defmodule Database.Repo.Migrations.CreateUserServers do
   use Ecto.Migration
 
   def change do
-
+    create table(:user_servers) do
+      add :host_id, :string
+      add :is_server_active, :boolean
+      add :ip, :string
+      add :user, :string
+      add :server_name, :string
+      add :creation_date, :utc_datetime
+      add :deleted_date, :utc_datetime
+  end
   end
 end
